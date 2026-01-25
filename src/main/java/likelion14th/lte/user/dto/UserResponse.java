bc564f8479e27196e16faa4fc86108bfb21972c8
dh1180 <mjdh1936@gmail.com>
2026-01-25 15:57:36 +1000

    feat: 유저 도메인(User) 및 리프레시 토큰(RefreshToken) 초기 구현
    - User 엔티티(users) 추가 및 Builder 생성자 구성
    - RefreshToken 엔티티(유저 1:1) 추가
    - UserResponse DTO 및 from(User) 변환 로직 추가

---
+++ src/main/java/likelion14th/lte/user/dto/UserResponse.java
@@ -0,0 +1,25 @@
+package likelion14th.lte.user.dto;
+
+import likelion14th.lte.user.domain.User;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class UserResponse {
+    private Long id;
+    private String username;
+    private String introduction;
+    private String profileImage;
+    private String userTag;
+
+    public static UserResponse from(User user) {
+        return UserResponse.builder()
+                .id(user.getId())
+                .username(user.getUsername())
+                .introduction(user.getIntroduction())
+                .profileImage(user.getProfileImage())
+                .userTag(user.getUserTag())
+                .build();
+    }
+}
